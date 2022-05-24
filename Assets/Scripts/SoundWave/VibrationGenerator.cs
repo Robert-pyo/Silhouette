d936e542271df2bdd577efb344dd51a3d2728282
RAGG <breativ@gmail.com>
2022-05-24 21:50:33 +0900

    Rope 구현

---
+++ Assets/Scripts/SoundWave/VibrationGenerator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VibrationGenerator : MonoBehaviour
+{
+    [SerializeField] private Transform m_genPoint;
+    [SerializeField] private float m_power;
+    [SerializeField] private float m_delayTime;
+
+    private Outline m_outline;
+
+    private void Awake()
+    {
+        m_outline = gameObject.AddComponent<Outline>();
+
+        m_outline.enabled = true;
+        m_outline.OutlineMode = Outline.Mode.SilhouetteOnly;
+        m_outline.OutlineColor = Color.blue;
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(GenerateVibration());
+    }
+
+    private IEnumerator GenerateVibration()
+    {
+        while (true)
+        {
+            SoundWaveManager.Instance.GenerateSoundWave(
+            transform, m_genPoint.position, m_genPoint.position, m_power);
+            yield return new WaitForSeconds(m_delayTime);
+        }
+    }
+}
